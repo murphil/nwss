9e68831b9f1a703fceda09ca4b44aa91da7ccd09
nash <nash@iffy.me>
2020-05-13 18:19:50 +0800

    ms

--- home/.zshrc.d/cmd_time.zsh
+++ home/.zshrc.d/cmd_time.zsh
@@ -1,5 +1,5 @@
 _command_time_preexec() {
-  timer=${timer:-$SECONDS}
+  timer=${timer:-$(($(date +%s%0N)/1000000))}
   ZSH_COMMAND_TIME_MSG=${ZSH_COMMAND_TIME_MSG-"Time: %s"}
   ZSH_COMMAND_TIME_COLOR=${ZSH_COMMAND_TIME_COLOR-"white"}
   export ZSH_COMMAND_TIME=""
@@ -7,8 +7,8 @@ _command_time_preexec() {
 
 _command_time_precmd() {
   if [ $timer ]; then
-    timer_show=$(($SECONDS - $timer))
-    if [ -n "$TTY" ] && [ $timer_show -ge ${ZSH_COMMAND_TIME_MIN_SECONDS:-3} ]; then
+    timer_show=$(($(($(date +%s%0N)/1000000)) - $timer))
+    if [ -n "$TTY" ] && [ $timer_show -ge ${ZSH_COMMAND_TIME_MIN_SECONDS:-3000} ] || [ -n "$timing" ]; then
       export ZSH_COMMAND_TIME="$timer_show"
       if [ ! -z ${ZSH_COMMAND_TIME_MSG} ]; then
         zsh_command_time
@@ -37,16 +37,17 @@ get_dot () {
 
 zsh_command_time() {
     if [ -n "$ZSH_COMMAND_TIME" ]; then
-        hours=$(($ZSH_COMMAND_TIME/3600))
-        min=$(($ZSH_COMMAND_TIME/60))
-        sec=$(($ZSH_COMMAND_TIME%60))
-        if [ "$ZSH_COMMAND_TIME" -le 60 ]; then
-            timer_show="$fg[green]$ZSH_COMMAND_TIME s."
-        elif [ "$ZSH_COMMAND_TIME" -gt 60 ] && [ "$ZSH_COMMAND_TIME" -le 180 ]; then
-            timer_show="$fg[yellow]$min min. $sec s."
+        hours=$(($ZSH_COMMAND_TIME/3600000))
+        min=$(($ZSH_COMMAND_TIME/60000%60))
+        sec=$(($ZSH_COMMAND_TIME/1000%60))
+        ms=$(($ZSH_COMMAND_TIME%1000))
+        if [ "$ZSH_COMMAND_TIME" -le 60000 ]; then
+            timer_show="$fg[green]$sec s. $ms ms."
+        elif [ "$ZSH_COMMAND_TIME" -gt 60000 ] && [ "$ZSH_COMMAND_TIME" -le 180000 ]; then
+            timer_show="$fg[yellow]$min min. $sec s. $ms ms."
         else
             if [ "$hours" -gt 0 ]; then
-                min=$(($min%60))
+                min=$(($min%60000))
                 timer_show="$fg[red]$hours h. $min min. $sec s."
             else
                 timer_show="$fg[red]$min min. $sec s."
